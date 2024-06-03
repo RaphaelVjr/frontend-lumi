35ed3cd05c2f2a8a0ba2ad2ce54eae7ffae39940
Raphael Vitorio <raphaelvitorio01@gmail.com>
2024-06-03 20:02:53 -0300

    endpoint changed

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -53,7 +53,7 @@ const Library = () => {
       render: (_, record) => (
         <motion.a
           className="bg-[#0dad62] text-[#12312a] rounded p-2 hover:text-white justify-center align-middle"
-          href={`${baseUrl}/invoices/download/${record.id}`}
+          href={`${baseUrl}/invoices/download/:id${record.id}`}
           download
         >
           Download
